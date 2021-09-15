4f0fa0c52793c626f807ff6a0e9d89281318e35c
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-09-15 16:06:06 +0700

    finish 'Find the odd int'

---
+++ 6_kyu/find_the_odd_int.py
@@ -0,0 +1,20 @@
+def find_it(seq):
+    uniques = set(seq)
+    for unique in uniques:
+        if seq.count(unique) % 2 == 1:
+            return unique
+    
+    # BEST
+    # for i in seq:
+    #     if seq.count(i)%2!=0:
+    #         return i
+
+
+
+print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]), 5)
+print(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]), -1);
+print(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]), 5);
+print(find_it([10]), 10);
+print(find_it([10, 10, 10]), 10);        
+print(find_it([1,1,1,1,1,1,10,1,1,1,1]), 10);
+print(find_it([5,4,3,2,1,5,4,3,2,10,10]), 1);
